65422152bee56133deb7987aa833709a80198302
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-02-17 15:19:08 -0500

    Add Date Class

---
+++ OPPConcepts/OPPConcepts.Fronted/Program.cs
@@ -0,0 +1,20 @@
+﻿using OPPConcepts.backed;
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(2026, 2, 29);
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
---
+++ OPPConcepts/OPPconcepts.Frontend/Program.cs
@@ -0,0 +1,9 @@
+﻿using OPPConcepts.backed;
+
+var date1 = new Date();
+var date2 = new Date(2026, 2, 12);
+
+Console.WriteLine(date1);
+Console.WriteLine(date2);
+
+
