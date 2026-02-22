2da86981cbcf7fec2f36334dfe89f11f37ade322
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-02-21 22:07:47 -0500

    Add Salary employee

--- OPPConcepts/OPPConcepts.backed/SalaryEmployee.cs
+++ OPPConcepts/OPPConcepts.backed/SalaryEmployee.cs
@@ -4,6 +4,36 @@ using System.Text;
 
 namespace OPPConcepts.backed;
 
-internal class SalaryEmployee
+public class SalaryEmployee : Employee
 {
+    // Fields
+    private decimal _salary;
+
+    // Constructors
+    public SalaryEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, decimal salary)
+        : base(id, firstName, lastName, isActive, bornDate, hireDate)
+    {
+        _salary = salary;
+    }
+
+    // Ptoperties
+    public decimal Salary
+    {
+        get => _salary;
+        set => _salary = ValidateSalary(value);
+    }
+
+    // Methods
+    public override decimal GetValueToPay() => Salary;
+    
+    public override string ToString() => base.ToString();
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 2000000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary must be greater than or equal to 2,000,000.");
+        }
+        return salary;
+    }
 }
