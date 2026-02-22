3e4d4099cc324b6f80bee01b64d7db73c666f32c
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-02-22 15:38:33 -0500

    Add Base Commision employee

--- OPPConcepts/OPPConcepts.Fronted/Program.cs
+++ OPPConcepts/OPPConcepts.Fronted/Program.cs
@@ -9,7 +9,7 @@ try
     employees.Add(employee1);
     var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1980, 3, 5), new Date(2016, 11, 16), 10395876);
     employees.Add(employee2);
-    var employee3 = new HourlyEmployee(3030, "Ana", "Lopez", true, new Date(1995, 8, 20), new Date(2022, 6, 1), 15000, 160);
+    var employee3 = new HourlyEmployee(3030, "Ana", "Lopez", true, new Date(1995, 8, 20), new Date(2022, 6, 1), 18000, 95);
     employees.Add(employee3);
     var employee4 = new HourlyEmployee(4040, "Kevin", "Rua", true, new Date(1998, 8, 20), new Date(2021, 6, 1), 10000, 112);
     employees.Add(employee4);
@@ -17,7 +17,11 @@ try
     employees.Add(employee5);
     var employee6 = new CommisionEmployee(6060, "Diego", "Martinez", true, new Date(1985, 7, 30), new Date(2010, 9, 1), 0.03f, 120000000);
     employees.Add(employee6);
-   
+    var employee7 = new BaseCommisionEmployee(7070, "Laura", "Garcia", true, new Date(1993, 11, 25), new Date(2019, 5, 20), 0.015f, 250000000, 550000);
+    employees.Add(employee7);
+    var employee8 = new BaseCommisionEmployee(8080, "Carlos", "Rodriguez", true, new Date(1988, 4, 10), new Date(2026, 2, 10), 0.015f, 0, 550000);
+    employees.Add(employee8);
+
     foreach (var employee in employees)
     {
         Console.WriteLine(new string('-', 47));
@@ -31,7 +35,6 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
 
 
 
---
+++ OPPConcepts/OPPConcepts.backed/BaseCommisionEmployee.cs
@@ -0,0 +1,36 @@
+﻿namespace OPPConcepts.backed;
+
+public class BaseCommisionEmployee : CommisionEmployee
+{
+    // Fields
+    private decimal _salary;
+
+    // Constructors
+    public BaseCommisionEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, float commisionPercent, decimal sales, decimal salary) :
+        base(id, firstName, lastName, isActive, bornDate, hireDate, commisionPercent, sales)
+    {
+        Salary = salary;
+    }
+
+    // Properties
+    public decimal Salary
+    {
+        get => _salary;
+        set => _salary = ValidateSalary(value);
+    }
+
+    // Methods
+    public override decimal GetValueToPay() => base.GetValueToPay() + Salary;
+
+    override public string ToString() => $"{base.ToString()}" +
+        $"\n\tSalary base......: {Salary,20:C2}";
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 500000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary base must be greater than or equal to $500,000.");
+        }
+        return salary;
+    }
+}
